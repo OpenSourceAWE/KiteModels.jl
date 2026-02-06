3742199888dc626d079aca7c89c653191c08f7ab
Uwe Fechner <fechner@aenarete.eu>
2026-02-06 12:18:43 +0100

    Enable Julia Formatter

--- examples/bench_4p.jl
+++ examples/bench_4p.jl
@@ -7,7 +7,8 @@ using Printf
 
 using Pkg
 if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
-    using TestEnv; TestEnv.activate()
+    using TestEnv;
+    TestEnv.activate()
 end
 using ControlPlots
 
@@ -15,24 +16,24 @@ set = deepcopy(load_settings("system.yaml"))
 
 # the following values can be changed to match your interest
 dt = 0.05
-set.solver="DFBDF"              # IDA or DFBDF
-set.linear_solver="GMRES"       # GMRES, LapackDense or Dense
+set.solver = "DFBDF"              # IDA or DFBDF
+set.linear_solver = "GMRES"       # GMRES, LapackDense or Dense
 STEPS = 200
 PRINT = false
 STATISTIC = false
-const PLOT=true
+const PLOT = true
 # end of user parameter section #
 
-kcu::KCU = KCU(set)
+kcu::KCU   = KCU(set)
 kps4::KPS4 = KPS4(kcu)
 
-v_time = zeros(STEPS)
+v_time  = zeros(STEPS)
 v_speed = zeros(STEPS)
 v_force = zeros(STEPS)
 
-function simulate(integrator, steps, offset=0)
+function simulate(integrator, steps, offset = 0)
     iter = 0
-    for i in 1:steps
+    for i = 1:steps
         acc = 0.0
         if kps4.t_0 > 3.0 + offset
             acc = 0.1
@@ -43,17 +44,17 @@ function simulate(integrator, steps, offset=0)
         set_speed = kps4.sync_speed+acc*dt
         if PRINT
             lift, drag = lift_drag(kps4)
-            @printf "%.2f: " round(integrator.t, digits=2)
+            @printf "%.2f: " round(integrator.t, digits = 2)
             println("lift, drag  [N]: $(round(lift, digits=2)), $(round(drag, digits=2))")
         end
 
-        next_step!(kps4, integrator; set_speed, dt=dt)
+        next_step!(kps4, integrator; set_speed, dt = dt)
         iter += kps4.iter
     end
     iter / steps
 end
 
-integrator = init!(kps4, delta=0, stiffness_factor=0.5, prn=STATISTIC)
+integrator = init!(kps4, delta = 0, stiffness_factor = 0.5, prn = STATISTIC)
 
 println("\nStarting simulation...")
 simulate(integrator, 100, 100)
@@ -64,7 +65,13 @@ speed = (STEPS-100) / runtime * dt
 println("Simulation speed: $(round(speed, digits=2)) times realtime.")
 if PLOT
     local p
-    p = plotx(v_time[1:STEPS-100], v_speed[1:STEPS-100], v_force[1:STEPS-100]; ylabels=["v_reelout  [m/s]","tether_force [N]"], fig="winch")
+    p = plotx(
+        v_time[1:(STEPS-100)],
+        v_speed[1:(STEPS-100)],
+        v_force[1:(STEPS-100)];
+        ylabels = ["v_reelout  [m/s]", "tether_force [N]"],
+        fig = "winch",
+    )
     display(p)
 end
 lift, drag = lift_drag(kps4)
