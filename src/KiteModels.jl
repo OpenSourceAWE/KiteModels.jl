8f25b85be870be61cd52f1e0d8a9e8ccabb4d5f3
Uwe Fechner <fechner@aenarete.eu>
2026-02-14 11:05:13 +0100

    Cleanup

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -655,8 +655,7 @@ function copy_examples()
 end
 
 function copy_model_settings()
-    files = ["settings.yaml", "ram_air_kite_body.obj", "ram_air_kite_foil.dat", "system.yaml", "settings_ram.yaml", 
-             "system_ram.yaml", "ram_air_kite_foil_cd_polar.csv", "ram_air_kite_foil_cl_polar.csv", "ram_air_kite_foil_cm_polar.csv"]
+    files = ["settings.yaml", "system.yaml"]
     dst_path = abspath(joinpath(pwd(), "data"))
     copy_files("data", files)
     set_data_path(joinpath(pwd(), "data"))
