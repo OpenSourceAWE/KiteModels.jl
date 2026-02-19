8eb740805efd38e6b83321e5d927984883cde452
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-02-19 06:40:38 +0100

    Fix comment

--- src/KPS4.jl
+++ src/KPS4.jl
@@ -642,7 +642,7 @@ function turn(res, upwind_dir)
 end
 
 """
-    find_steady_state!(s::KPS4; prn=false, delta = 0.01, stiffness_factor=0.035, upwind_dir=-pi/2))
+    find_steady_state!(s::KPS4; prn=false, delta = 0.001, stiffness_factor=0.035, upwind_dir=-pi/2))
 
 Find an initial equilibrium, based on the initial parameters
 `l_tether`, elevation and `v_reel_out`.
