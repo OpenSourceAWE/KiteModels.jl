36dac0114f46fa29e313b128ba7ed417a15fcdc9
Uwe Fechner <fechner@aenarete.eu>
2026-02-22 17:17:23 +0100

    Fix menu2.jl

--- examples/menu2.jl
+++ examples/menu2.jl
@@ -2,13 +2,22 @@
 # SPDX-License-Identifier: MIT
 using REPL.TerminalMenus
 
-options = ["plot_cl_cd_plate = include(\"plot_cl_cd_plate.jl\")",
+options =  if haskey(ENV, "USE_V9")
+          ["plot_cl_cd_plate = include(\"plot_cl_cd_plate.jl\")",
            "plot_side_cl = include(\"plot_side_cl.jl\")",
            "steering_test_4p = include(\"steering_test_4p.jl\")",
            "plot_parking_test = include(\"plot_parking_test.jl\")",
            "calculate_rot_inertia = include(\"calculate_rotational_inertia.jl\")",
            "build_docu = include(\"../scripts/build_docu.jl\")",
            "quit"]
+else
+          ["plot_cl_cd_plate = include(\"plot_cl_cd_plate.jl\")",
+           "plot_side_cl = include(\"plot_side_cl.jl\")",
+           "steering_test_4p = include(\"steering_test_4p.jl\")",
+           "calculate_rot_inertia = include(\"calculate_rotational_inertia.jl\")",
+           "build_docu = include(\"../scripts/build_docu.jl\")",
+           "quit"]
+end
 
 function extra_menu()
     active = true
