4c2c35f971ecbd646b10b0155f8cb798b0eacd14
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-02 22:32:19 +0100

    Cleanup

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,38 +1,6 @@
 # Copyright (c) 2024 Uwe Fechner, Bart van de Lint
 # SPDX-License-Identifier: MIT
 
-function decompress_binary(infile, outfile; chunksize=4096)
-    open(infile) do input
-        open(outfile, "w") do output
-            stream = XzDecompressorStream(input)
-            while !eof(stream)
-                write(output, read(stream, chunksize))
-            end
-        end
-    end
-end
-
-function filecmp(path1::AbstractString, path2::AbstractString)
-    stat1, stat2 = stat(path1), stat(path2)
-    if !(isfile(stat1) && isfile(stat2)) || filesize(stat1) != filesize(stat2)
-        return false
-    end
-    open(path1, "r") do file1
-        open(path2, "r") do file2
-            buf1 = Vector{UInt8}(undef, 32768)
-            buf2 = similar(buf1)
-            while !eof(file1) && !eof(file2)
-                n1 = readbytes!(file1, buf1)
-                n2 = readbytes!(file2, buf2)
-                if n1 != n2 || buf1[1:n1] != buf2[1:n2]
-                    return false
-                end
-            end
-            return eof(file1) && eof(file2)
-        end
-    end
-end
-
 @setup_workload begin
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
