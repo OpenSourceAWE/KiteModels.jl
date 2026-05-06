dc935d948cc0cb81d310b79f75f35028e2067fca
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-05-06 20:14:55 +0200

    Remove last_roll

--- src/KPS4.jl
+++ src/KPS4.jl
@@ -126,8 +126,6 @@ $(TYPEDFIELDS)
     pitch::S =            0.0
     "pitch rate [rad/s]"
     pitch_rate::S =       0.0
-    "roll angle from previous step [rad]"
-    last_roll::S =        0.0
     "aoa at particle B"
     alpha_2::S =           0.0
     "aoa at particle C"
@@ -245,7 +243,6 @@ function clear!(s::KPS4)
     s.kcu.set_steering = 0.0
     s.pitch = 0.0
     s.pitch_rate = 0.0
-    s.last_roll = 0.0
     s.alpha_depower = 0.0
     s.depower = 0.0
     s.steering = 0.0
